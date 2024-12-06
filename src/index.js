9a25b0d26c018c19e81948b34998b438f35a50a7
GhoriHet <121792221+GhoriHet@users.noreply.github.com>
2024-12-06 17:35:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 
 const connectMongoDatabase = require('./database/mongoDB_index');
 connectMongoDatabase();
@@ -8,12 +8,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World! 😁 🎈');
-});
-
 const routs = require('./routes/routes_index');
 app.use('/api', routs);
+
+// Fallback for 404 errors
+app.use('*', (req, res) => {
+    res.status(404).send('Page not found');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`* Server Start On Port ${port}`));
